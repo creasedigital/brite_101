ad2ad900dec474c4a0f6fb5df9dca36d857eb363
Ojore Chris <ojorechris@gmail.com>
2023-03-11 11:56:35 +0100

    demo

---
+++ js/index.js
@@ -0,0 +1,7 @@
+// Code to handle form submission
+/* const form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
+	event.preventDefault();
+	alert("Thank you for your message!");
+});
+ */
